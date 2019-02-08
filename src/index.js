23ee54c65aef724d82251abbd4ba409782305334
emma-hunt <emma.g.hunt@outlook.com>
2019-02-08 15:47:04 -0700

    ported results.js code from CodePen

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Results from './Results';
+import Banner from './Results';
+import Filters from './Results';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Banner />, document.getElementById("banner"));
+ReactDOM.render(<Filters />, document.getElementById("filters"));
+ReactDOM.render(<Results />, document.getElementById("results"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
